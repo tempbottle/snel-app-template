6e3abbc9f257a1867c4f584b31f7b2d0f815ea8c
tempbottle <xjp@xjump.local>
2021-12-11 00:32:27 +0800

    update

---
+++ app1/src/components/Home.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let name;
+</script>
+
+<h1>Hello {name}!</h1>
+<p>
+  Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how
+  to build Svelte apps.
+</p>
+
+<link
+  href="/static/google-fonts.css" 
+  rel="stylesheet"
+/>
+
+<style>
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+</style>
---
+++ app1/src/main.js
@@ -0,0 +1,6 @@
+import App1 from "./App1.svelte";
+
+const app1 = new App1({
+  target: document.querySelector("#__snel"),
+  props: {},
+});
